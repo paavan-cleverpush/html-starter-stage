68849afdddc91b0e88ec322c3d4ab0c3c51e33a3
paavan-cleverpush <p.khunt@cleverpush.com>
2025-03-27 10:50:55 +0530

    code fix

--- cleverpush-worker.js
+++ cleverpush-worker.js
@@ -1,1 +1,6 @@
-importScripts('https://static.cleverpush.com/channel/worker/GBLTKej2jdk4zrnKP.js' + self.location.search);
+// importScripts('https://static.cleverpush.com/channel/worker/GBLTKej2jdk4zrnKP.js' + self.location.search);
+self.cleverpushConfig = {
+  channelId: 'GBLTKej2jdk4zrnKP',
+  apiEndpoint: 'https://api-stage.cleverpush.com',
+};
+importScripts("https://static.cleverpush.com/sdk/cleverpush-worker.stage.js");
